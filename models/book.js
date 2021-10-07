998b06263d8cbef468a84b166ef850ec357ee5f0
Andreas Febryanto <andreas.febryanto@gmail.com>
2021-10-07 09:09:54 +0700

    add error handler, validation sequelize

---
+++ models/book.js
@@ -0,0 +1,40 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Book extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Book.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notEmpty: { msg: "Book name field must be filled" },
+          notNull: { msg: "Book name field must be filled" },
+        },
+      },
+      genre: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notEmpty: { msg: "Genre field must be filled" },
+          notNull: { msg: "Genre field must be filled" },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Book",
+    }
+  );
+  return Book;
+};
